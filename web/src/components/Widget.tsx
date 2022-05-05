e82a94612b2e6e2a47078fffb1a353a54a2fd9c6
diego64 <diegoferreira1964@gmail.com>
2022-05-04 22:42:30 -0300

    Finalizando o front-end

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,6 +1,6 @@
-import { ChatTeardropDots } from 'phosphor-react';
-import { Popover } from '@headlessui/react';
-import { WidgetForm } from './WidgetForm';
+import { ChatTeardropDots } from 'phosphor-react'
+import { Popover } from '@headlessui/react'
+import { WidgetForm } from './WidgetForm'
 
 export function Widget() {
   return (
@@ -9,14 +9,16 @@ export function Widget() {
         <WidgetForm />
       </Popover.Panel>
 
-        <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
-          <ChatTeardropDots className="w-6 h-6"/>
+      <Popover.Button 
+        className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group"
+      >
+        <ChatTeardropDots className="w-6 h-6" />
 
-          <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
-            <span className="pl-2"></span>
-            Feedback
-          </span>
-        </Popover.Button>
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+          <span className="pl-2"></span>
+          Feedback
+        </span>
+      </Popover.Button>
     </Popover>
   )
 }
